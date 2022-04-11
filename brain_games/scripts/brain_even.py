bb537f47dc7330ca2100c24f8fdc10fc4d5cf25d
AlexMusin <alex@DESKTOP-DLM2U3K.localdomain>
2022-04-11 20:45:53 +0300

    Step 6. Calc game and logic changes: common logic is moved out to game_logic.py

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,20 +1,9 @@
-#!/usr/bin/env python
 """Even game launch."""
 
-from brain_games.games import err_exit, game_even, greet
+from brain_games import game_logic
 
 
 def main():
     """Play odd-or-even game."""
-    username = greet.greeting()
-    print(game_even.rules)
-    index = 0
-    while index < 3:
-        condition, answer = game_even.even()
-        if condition == answer:
-            print('Correct!')
-            index += 1
-        else:
-            err_exit.error()
-    if index == 3:
-        print('Congratulations, {0}!'.format(username))
+    game_name = 'even'
+    game_logic.logic(game_name)
