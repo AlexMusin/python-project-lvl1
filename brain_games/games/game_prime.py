1f47abd4fdccf61b5061908642c3fd444e1e13d3
AlexMusin <alex@DESKTOP-DLM2U3K.localdomain>
2022-04-14 15:46:23 +0300

    Linter fixes.

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -15,7 +15,7 @@ def run_game():
     is_prime = 1
     index = 2
     modifier = 2 if given_number < 3 else 0
-    while is_prime == 1 and index <= int(given_number / 2) + modifier:  # use modifier to pass while cycle in '1' and '2' cases 
+    while is_prime == 1 and index <= int(given_number / 2) + modifier:
         if not given_number % index:
             is_prime = 0
         index += 1
