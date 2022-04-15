a89d2d18df9733fb4521a9a84c9f6f5e873e0dce
AlexMusin <alex@DESKTOP-DLM2U3K.localdomain>
2022-04-15 13:35:19 +0300

    Minor_fixes

--- brain_games/games/game_calc.py
+++ brain_games/games/game_calc.py
@@ -14,7 +14,7 @@ def run_game():
     """Return variables for game logic."""
     number1 = random.randint(LEFT_LIMIT, RIGHT_LIMIT)
     number2 = random.randint(LEFT_LIMIT, RIGHT_LIMIT)
-    action = random.choice(action_list)
+    action = random.choice(ACTION_LIST)
     cond = str(eval('{0} {2} {1}'.format(number1, number2, action)))
     print('Question: {0} {2} {1}'.format(number1, number2, action))
     answer = prompt.string('Your answer: ')
