78a6f1cc38700817a8b615b36b3c7979319f80a2
AlexMusin <alex@DESKTOP-DLM2U3K.localdomain>
2022-04-15 13:52:50 +0300

    Minor_fixes 2

--- brain_games/err_exit.py
+++ brain_games/err_exit.py
@@ -6,8 +6,9 @@ def error(cond, answer, username):
     """Say error and exit."""  # noqa: DAR101
     print("'{0}' is wrong answer ;(. Correct answer was '{1}.'".format(answer, cond))  # noqa: E501
     incorrect = "Let's try again, {0}!".format(username)
-    sys.exit(incorrect)
-
+    #sys.exit(incorrect)
+    print(incorrect)
+    exit(0)
 
 if __name__ == '__main__':
     error()
