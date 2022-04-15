9315fbf2367d153b72efb16ff10cdafe57a34600
AlexMusin <alex@DESKTOP-DLM2U3K.localdomain>
2022-04-15 13:45:36 +0300

    Minor_fixes 2

--- brain_games/greet.py
+++ brain_games/greet.py
@@ -6,7 +6,7 @@ def greeting():
     """Greet the Player."""
     print('Welcome to the Brain Games!')
     username = prompt.string('May I have your name? ')
-    greet = ('Hello, {0}'.format(username))
+    greet = ('Hello, {0}!'.format(username))
     print(greet)
     return (username)
 
