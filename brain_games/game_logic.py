a65acb5378ffd726c04bb1aff841148397fd51fd
AlexMusin <alersky21@gmail.com>
2022-04-21 20:59:08 +0300

    Insert greeting and error_exit logic inside run_logic

--- brain_games/game_logic.py
+++ brain_games/game_logic.py
@@ -7,27 +7,12 @@ import prompt
 ROUNDS = 3
 
 
-def greeting():
-    """Greet the Player."""
+def run_logic(game):
+    """Run game logic."""
     print('Welcome to the Brain Games!')
     username = prompt.string('May I have your name? ')
     greet = ('Hello, {0}!'.format(username))
     print(greet)
-    return (username)
-
-
-def error(condition, answer, username):
-    """Say error and exit."""
-    expr = ' is wrong answer ;(. Correct answer was '
-    print("'{0}'{2}'{1}'.".format(answer, condition, expr))
-    incorrect = "Let's try again, {0}!".format(username)
-    print(incorrect)
-    sys.exit(0)
-
-
-def run_logic(game):
-    """Run game logic."""
-    username = greeting()
     rules = game.RULES
     print(rules)
     index = 0
@@ -39,7 +24,10 @@ def run_logic(game):
             print('Correct!')
             index += 1
         else:
-            error(condition, answer, username)
+            expr = ' is wrong answer ;(. Correct answer was '
+            print("'{0}'{2}'{1}'.".format(answer, condition, expr))
+            incorrect = "Let's try again, {0}!".format(username)
+            print(incorrect)
             break
     else:
         print('Congratulations, {0}!'.format(username))
