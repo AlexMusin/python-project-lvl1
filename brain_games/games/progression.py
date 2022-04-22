a63d73293b06503d1a2bbb9ca91fc434c452ba70
AlexMusin <alersky21@gmail.com>
2022-04-22 11:35:08 +0300

    Shorten variables in progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,15 +5,15 @@ import random
 RULES = 'What number is missing in the progression?'
 LEFT_LIMIT = -25
 RIGHT_LIMIT = 25
-PROGRESSION_LENGTH_MIN = 5
-PROGRESSION_LENGTH_MAX = 12
+PROGR_LENGTH_MIN = 5
+PROGR_LENGTH_MAX = 12
 STEP_MIN = -10
 STEP_MAX = 10
 
 
 def make_progression():
     """Generate and return progression."""
-    progression_length = random.randint(PROGRESSION_LENGTH_MIN, PROGRESSION_LENGTH_MAX)
+    progression_length = random.randint(PROGR_LENGTH_MIN, PROGR_LENGTH_MAX)
     step = random.randint(STEP_MIN, STEP_MAX)
     progression = []
     element = random.randint(LEFT_LIMIT, RIGHT_LIMIT)
@@ -27,10 +27,10 @@ def run_game():
     """Return variables for game logic."""
     progression, progression_length = make_progression()
     question_index = random.randint(0, progression_length - 1)
-    conditionition = str(progression[question_index])
+    condition = str(progression[question_index])
     progression[question_index] = '..'
     question = (' '.join(map(str, progression)))
-    return (question, conditionition)
+    return (question, condition)
 
 
 if __name__ == '__main__':
