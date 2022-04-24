47114de382a0cd28d6457d462da8b702c46da569
AlexMusin <alersky21@gmail.com>
2022-04-24 16:00:20 +0300

    refactor gcd.py

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -7,24 +7,29 @@ RIGHT_LIMIT = 25
 RULES = 'Find the greatest common divisor of given numbers.'
 
 
-def find_gcd():
-    """Generate two non-zero integers and finds their GCD."""
-    counter = 0
+def generate_input():
     counter2 = 0
     number1 = 1
     number2 = 1
-    while counter == 0:
-        while number1 != 0 and number2 != 0 and counter2 == 0:
+    while number1 != 0 and number2 != 0 and counter2 == 0:
             number1 = random.randint(LEFT_LIMIT, RIGHT_LIMIT)
             number2 = random.randint(LEFT_LIMIT, RIGHT_LIMIT)
             counter2 += 1
-        index = 1
+    return (number1, number2)
+
+
+def find_gcd():
+    """Generate two non-zero integers and finds their GCD."""
+    counter = 0
+    number1, number2 = generate_input()
+    while counter == 0:
+        divisor = 1
         divisor_list = []
-        while index <= min(abs(number1), abs(number2)):
-            if number1 % index == 0 and number2 % index == 0:
-                divisor_list.append(index)
-                counter = index
-            index += 1
+        while divisor <= min(abs(number1), abs(number2)):
+            if number1 % divisor == 0 and number2 % divisor == 0:
+                divisor_list.append(divisor)
+                counter = divisor
+            divisor += 1
     gcd = str(max(divisor_list))
     return ((number1, number2), gcd)
 
