afaa90c88456427c697eb6dab094f44bb57e8dec
AlexMusin <alersky21@gmail.com>
2022-04-24 19:13:25 +0300

    add game_engine.py to repository

---
+++ brain_games/game_engine.py
@@ -0,0 +1,26 @@
+"""Brain-game engine."""
+
+import prompt
+
+ROUNDS = 3
+
+
+def run(game):
+    """Run game logic."""
+    print('Welcome to the Brain Games!')
+    username = prompt.string('May I have your name? ')
+    print('Hello, {0}!'.format(username))
+    print(game.RULES)
+    for index in range(0, ROUNDS):
+        question, right_answer = game.run_round()
+        print('Question: {0}'.format(question))
+        user_answer = prompt.string('Your answer: ')
+        if right_answer == user_answer:
+            print('Correct!')
+            index += 1
+        else:
+            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{right_answer}'.")
+            print("Let's try again, {0}!".format(username))
+            break
+    else:
+        print('Congratulations, {0}!'.format(username))
