0812eb94e159c3ac84b2e20348cd8e3e3c2dfff1
AlexMusin <alersky21@gmail.com>
2022-05-04 20:33:51 +0300

    Update progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -12,25 +12,25 @@ STEP_MAX = 10
 
 
 def make_progression():
-    """Generate and return progression."""
+    """Generate and return progression and its length."""
     progression_length = random.randint(PROGR_LENGTH_MIN, PROGR_LENGTH_MAX)
     step = random.randint(STEP_MIN, STEP_MAX)
-    progression = []
     element = random.randint(LEFT_LIMIT, RIGHT_LIMIT)
+    progression = []
     while len(progression) <= progression_length:
         progression.append(element)
         element += step
-    return (progression, progression_length)
+    question_index = random.randint(0, progression_length - 1)
+    right_answer = str(progression[question_index])
+    progression[question_index] = '..'
+    progression = str(progression)
+    return (progression, right_answer)
 
 
 def run_round():
     """Return variables for game logic."""
-    progression, progression_length = make_progression()
-    question_index = random.randint(0, progression_length - 1)
-    right_answer = str(progression[question_index])
-    progression[question_index] = '..'
-    question = (' '.join(map(str, progression)))
-    return (question, right_answer)
+    progression, right_answer = make_progression()
+    return (progression, right_answer)
 
 
 if __name__ == '__main__':
