44c4eff41f6072f0e23e13c96f97c333429b05af
AlexMusin <alersky21@gmail.com>
2022-05-07 17:03:39 +0300

    Updt even.py

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -11,13 +11,5 @@ RIGHT_LIMIT = 255
 def run_round():
     """Return even-game variables."""
     number = random.randint(LEFT_LIMIT, RIGHT_LIMIT)
-    right_answer = number % 2 == 1
-    if right_answer == 0:
-        right_answer = 'yes'
-    elif right_answer == 1:
-        right_answer = 'no'
+    right_answer = 'no' if number % 2 == 1 else 'yes'
     return (number, right_answer)
-
-
-if __name__ == '__main__':
-    run_round()
