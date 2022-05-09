dcee5adb8b7dc3615b6e84514a1fba0714360afb
AlexMusin <alersky21@gmail.com>
2022-05-09 18:42:39 +0300

    Updt cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -7,12 +7,3 @@ def welcome_user():
     """Ask username."""
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
-
-
-def main():
-    """Define main func."""
-    welcome_user()
-
-
-if __name__ == '__main__':
-    main()
